3a292103bbf1ac588f9fd8bdb07272dc52d8f7f6
Sugam Pokhrel <trovalds@linux-foundation.org>
2023-08-11 11:41:38 +0545

    removed the env bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Navbar from "../Components/Navbar"
 import Search from "../Components/searchField"
 import Images from "../Components/Images"
 import useAxios from "../Hooks/useAxios"
-
 function App() {
 const {response, isLoading, error, fetchData} =
- useAxios(`search/photos?page=1&query=office&client_id=${process.env.REACT_APP_UNSPLASH_KEY}`);
+ useAxios(`search/photos?page=1&query=office&client_id=${import.meta.env.REACT_APP_UNSPLASH_KEY}`);
+
 console.log(response);
   return (
 
